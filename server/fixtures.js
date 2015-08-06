14fea4767e02559d807ca95ff5c25288787890d3
thtruo <ttruongchau@gmail.com>
2015-08-06 01:02:51 -0700

    added fixture data

---
+++ server/fixtures.js
@@ -0,0 +1,74 @@
+if (DataTable.find().count() === 0) {
+  DataTable.insert({
+    "market": "AR-AE",
+    "metric": "ArPageViewsPerUser",
+    "average": 1.170973965,
+    "observedN": 125446,
+    "stdDev": 7.563560872,
+    "week": 1
+  });
+  DataTable.insert({
+    "market": "EN-SG",
+    "metric": "SearchTransfersPerUser",
+    "average": 0.997098172,
+    "observedN": 120269,
+    "stdDev": 6.922302057,
+    "week": 1
+  });
+  DataTable.insert({
+    "market": "EN-SG",
+    "metric": "WholePageClicksPerUser",
+    "average": 4.899516916,
+    "observedN": 120269,
+    "stdDev": 29.9317811,
+    "week": 1
+  });
+  DataTable.insert({
+    "market": "EN-US",
+    "metric": "ArPageViewsPerUser",
+    "average": 3.855664898,
+    "observedN": 12412192,
+    "stdDev": 17.84703366,
+    "week": 1
+  });
+  DataTable.insert({
+    "market": "EN-US",
+    "metric": "ArWholePageClicksPerUser",
+    "average": 1.26038487,
+    "observedN": 12412192,
+    "stdDev": 7.882498447,
+    "week": 1
+  });
+  DataTable.insert({
+    "market": "EN-US",
+    "metric": "GaPageViewsPerUser",
+    "average": 1.362620881,
+    "observedN": 12412192,
+    "stdDev": 9.346895613,
+    "week": 1
+  });
+  DataTable.insert({
+    "market": "ES-AR",
+    "metric": "GaWholePageClicksPerUser",
+    "average": 0.418222582,
+    "observedN": 822024,
+    "stdDev": 2.258572572,
+    "week": 1
+  });
+  DataTable.insert({
+    "market": "ES-CL",
+    "metric": "SearchTransfersPerUser",
+    "average": 0.550627047,
+    "observedN": 322703,
+    "stdDev": 5.505490403,
+    "week": 1
+  });
+  DataTable.insert({
+    "market": "ZH-TW",
+    "metric": "WholePageClicksPerUser",
+    "average": 4.390649756,
+    "observedN": 130649,
+    "stdDev": 14.12798132,
+    "week": 4
+  });
+}
