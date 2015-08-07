e7ff94076bafbe59794a86631ccf0634c8b6db53
thtruo <ttruongchau@gmail.com>
2015-08-06 23:18:03 -0700

    updated Metric component to select options from DataTable

--- client/components/Metric.jsx
+++ client/components/Metric.jsx
@@ -1,28 +1,45 @@
 /*
- * Metric component represents the metric form selection field
- * in the calculator app
+ * Metric component represents the metric dropdown form and is composed of
+ * FormSelect components.
  */
 Metric = React.createClass({
+  mixins: [ReactMeteorData],
+
+  // Loads items onto this.data.dataTable from the DataTable collection
+  getMeteorData() {
+    var distinctMetrics = _.uniq(DataTable.find({}, {
+      sort: {metric: 1},
+      fields: {metric: true}
+    }).fetch().map(function(x) {
+      return x.metric;
+    }), true);
+    return {
+      dataTableMetrics: distinctMetrics
+    }
+  },
   getInitialState: function() {
     return {value: 'NumVisits'};
   },
+
   handleChange: function(event) {
     this.setState({value: event.target.value});
   },
+
+  renderFormSelects() {
+    // Get tasks from this.data.dataTableMetrics
+    return this.data.dataTableMetrics.map((metric, key) => {
+      return <FormSelect key={key} value={metric} />;
+    });
+  },
+
   render() {
-    var value = this.state.value;
     return (
       <div className="field">
-        <label>Metric</label>
-        <div className="ui left icon input">
-          <i className="asterisk icon"></i>
-          <input
-            type="text"
-            name="metric"
-            placeholder="Select one or more metrics"
-            onChange={this.handleChange}
-          />
-        </div>
+        <label>Market</label>
+        <select name="metrics" className="ui selection dropdown" multiple="" id="multi-select">
+          <option value="">Select your metrics</option>
+          {this.renderFormSelects()}
+        </select>
       </div>
     );
   }
